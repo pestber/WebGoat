1289b4e5ce68036962ef47961a4c7b664ce86cf0
pestber <p.esteban.2021@alumnos.urjc.es>
2024-04-04 18:02:51 +0200

    Revert "JWT Signature Verified"
    This reverts commit 1550a3817b15cf8a670875a56a7a250dbef1a18f.

--- src/main/java/org/owasp/webgoat/lessons/jwt/JWTRefreshEndpoint.java
+++ src/main/java/org/owasp/webgoat/lessons/jwt/JWTRefreshEndpoint.java
@@ -24,8 +24,12 @@ package org.owasp.webgoat.lessons.jwt;
 
 import static org.springframework.http.ResponseEntity.ok;
 
-import io.jsonwebtoken.*;
-
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Header;
+import io.jsonwebtoken.Jwt;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
@@ -80,10 +84,10 @@ public class JWTRefreshEndpoint extends AssignmentEndpoint {
     Map<String, Object> claims = Map.of("admin", "false", "user", user);
     String token =
         Jwts.builder()
-                .setIssuedAt(new Date(System.currentTimeMillis() + TimeUnit.DAYS.toDays(10)))
-                .setClaims(claims)
-                .signWith(SignatureAlgorithm.HS256, JWT_PASSWORD)
-                .compact();
+            .setIssuedAt(new Date(System.currentTimeMillis() + TimeUnit.DAYS.toDays(10)))
+            .setClaims(claims)
+            .signWith(io.jsonwebtoken.SignatureAlgorithm.HS512, JWT_PASSWORD)
+            .compact();
     Map<String, Object> tokenJson = new HashMap<>();
     String refreshToken = RandomStringUtils.randomAlphabetic(20);
     validRefreshTokens.add(refreshToken);
@@ -100,7 +104,7 @@ public class JWTRefreshEndpoint extends AssignmentEndpoint {
       return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
     }
     try {
-      Jwt jwt = (Jwt) Jwts.parser().setSigningKey(JWT_PASSWORD).parseClaimsJwt(token).getBody();
+      Jwt jwt = Jwts.parser().setSigningKey(JWT_PASSWORD).parse(token.replace("Bearer ", ""));
       Claims claims = (Claims) jwt.getBody();
       String user = (String) claims.get("user");
       if ("Tom".equals(user)) {
